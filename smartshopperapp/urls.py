38f1c4503524a25fd86fbdda09a75a4dc9a46f58
zionhaye <zionhayeofficial@gmail.com>
2022-03-06 18:21:29 -0400

    Set Main Url

--- smartshopperapp/urls.py
+++ smartshopperapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('Home/' , views.display_home_page ),
+    path('' , views.display_home_page ),
     path('CreateList/' , views.display_create_list_page),
     path('SelectCategory/', views.display_category_page),
     path('Catalogue/<category>/' , views.display_catalogue_page)
