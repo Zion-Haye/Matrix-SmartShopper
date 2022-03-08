f8a542ea3d512f36cd708f44cf19584f22795784
zionhaye <zionhayeofficial@gmail.com>
2022-03-07 23:33:05 -0400

    Authentication Setup V1

--- smartshopperapp/urls.py
+++ smartshopperapp/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('' , views.display_home_page ),
-    path('CreateList/' , views.display_create_list_page),
-    path('SelectCategory/', views.display_category_page),
-    path('Catalogue/<category>/' , views.display_catalogue_page)
+    path('' , views.display_home_page , name="home" ),
+    path('CreateList/' , views.display_create_list_page,name="createlist"),
+    path('SelectCategory/', views.display_category_page, name="selectcategory"),
+    path('Catalogue/<category>/' , views.display_catalogue_page, name="catalogue"),
+    path('Login/', views.display_login_page, name="login"),
+    path('SignUp/',views.display_signup_page, name="signup"),
+    path('Logout/', views.logout_user , name="logout"),
+    path('Accounts/',views.display_account_page, name="accounts"),
+    path('SearchResults/',views.display_search_results_page, name="searchresults")
     
 ]
--- smartshopperapp/views.py
+++ smartshopperapp/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
+import email
+from django.shortcuts import render , redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 def display_home_page(request):
+    isauthenticated = request.user.is_authenticated
+    print("In Home:")
+    print("Is Authenticated: ", isauthenticated)
     return render (request, 'home.html')
 
 def display_create_list_page(request):
-    return render  (request , 'createlist.html')
+
+    if request.method=="POST":
+        listname = request.POST.get('listname')
+        description = request.POST.get('description')
+       
+
+        print("In Create List Post Request")
+        print("listname: ",listname)
+        print("description: ",description)
+
+        return  redirect ('/SelectCategory/')
+    
+    else:
+
+        return render  (request , 'createlist.html')
 
 def display_category_page(request):
     return render (request , 'selectcategory.html' )
@@ -22,3 +43,79 @@ def display_catalogue_page(request , category):
 
     #Look into emplate respone
     return render (request , 'catalogue.html')
+
+def display_search_results_page(request):
+    return render (request, 'searchresult.html')
+
+def display_account_page(request):
+    return render (request, 'accounts.html')
+
+
+def display_signup_page(request):
+
+    if request.method=="POST":
+        username = request.POST.get('username')
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        email = request.POST.get('email')
+        password =  request.POST.get('password')
+
+        print("In SignUp Post Request")
+        print("Username: ",username)
+        print("First Name: ",firstname)
+        print("Last Name: ",lastname)
+        print("Email: ",email)
+
+        try:
+            thisuser = User.objects.create_user(username, email , password)
+            thisuser.first_name = firstname
+            thisuser.last_name = lastname 
+
+            thisuser.save()
+
+            messages.success(request, "Account Succesfully Created")
+
+            return redirect('/Login/')
+        except:
+            print("In SignuUp exception")
+            return redirect('/SignUp/')
+    
+    else:
+        print("Out Signup- Post request")
+
+        return render(request,'signup.html')
+
+def display_login_page(request):
+
+    if request.method=="POST":
+        username = request.POST.get('username')
+        password =  request.POST.get('password')
+
+        print("In Login Post Request")
+        print("Username: ",username)
+
+        try:
+            user = authenticate(username=username , password=password)
+
+            if user is not None:
+                login(request, user)
+                firstname = user.first_name
+                lastname = user.last_name
+
+            
+                return render(request, "createlist.html" , {"firstname":firstname})
+            
+            else:
+                messages.error(request,"Incorrect Credentials")
+                return redirect ('/Login/')
+
+        except:
+                print("In Login Exception")
+
+    return render (request,'login.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,"Logged Out Successfully!")
+    return redirect("/")
+    
