d397bfaf3912f564704bf700f387925a3f621d46
Zion-Haye <zionhayeofficial@gmail.com>
2022-04-11 00:33:25 -0400

    Model Test v1

---
+++ smartshopperapp/tests/test_views.py
@@ -0,0 +1,9 @@
+import django
+from  django.test import TestCase, Client
+from  django.urls import reverse
+from  smartshopperapp.models import Product, Configuration , List , ListItem
+import json
+
+class TestViews(TestCase):
+    pass
+
