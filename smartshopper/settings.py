de2838ff75d938ee0ac5ebdda0cf89fe212a2570
Zion-Haye <zionhayeofficial@gmail.com>
2022-04-17 13:54:34 -0400

    safe Point

--- smartshopper/settings.py
+++ smartshopper/settings.py
@@ -24,11 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2=q5vuc*gtvi2k=9j#kqc7)=3pz^98i^p_%rw9u@wj+jl1euud'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = False
 
-ALLOWED_HOSTS = ['matrix-smartshopper.herokuapp.com' , '127.0.0.1']
-#ALLOWED_HOSTS = []
+DEBUG = True
+#DEBUG = False
+
+#ALLOWED_HOSTS = ['matrix-smartshopper.herokuapp.com' , '127.0.0.1']
+ALLOWED_HOSTS = []
 
 # Application definition
 
