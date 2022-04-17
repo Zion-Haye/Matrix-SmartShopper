95d67af075edb9ecd911109ce31b2798040bfbbb
Zion-Haye <zionhayeofficial@gmail.com>
2022-04-17 02:52:44 -0400

    TEST Deployment 3

--- smartshopper/settings.py
+++ smartshopper/settings.py
@@ -24,10 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2=q5vuc*gtvi2k=9j#kqc7)=3pz^98i^p_%rw9u@wj+jl1euud'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
-#ALLOWED_HOSTS = ['matrix-smartshopper.herokuapp.com' , '127.0.0.1']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['matrix-smartshopper.herokuapp.com' , '127.0.0.1']
+#ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,9 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-#
-#STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-#
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 
 STATIC_URL = '/static/'
 
@@ -137,3 +139,7 @@ MEDIA_URL = '/images/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
